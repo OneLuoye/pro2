c74bb3d2477a1ce2a246c2b4fd2ce7449da248a5
lyf <2935255848@qq.com>
2019-07-29 15:56:06 +0800

    first add file

--- src/org/tar/lanqiao/util/StringUtil.java
+++ src/org/tar/lanqiao/util/StringUtil.java
@@ -3,6 +3,7 @@ package org.tar.lanqiao.util;
 public class StringUtil {
     public static  String valueOf(){
         System.out.print(0);
+        System.out.print(1);
         return null;
     }
 }
